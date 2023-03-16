11859092aa6d5da1358a560aeb290a7c01520cab
nazmieievilya <ilyanazmeev111@gmail.com>
2023-03-16 12:50:52 +0300

    added drawing animations

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const resultBox = document.querySelector(".result");
 const playerTitle = document.querySelector("h1");
 const background = document.querySelector("body");
 const replayBtn = document.querySelector("button");
+
+const o = `<svg id='o' xmlns="http://www.w3.org/2000/svg" width="268" height="257" viewBox="0 0 268 257" fill="none">
+<path d="M262 128.5C262 195.92 204.932 251 134 251C63.0681 251 6 195.92 6 128.5C6 61.0796 63.0681 6 134 6C204.932 6 262 61.0796 262 128.5Z" stroke="white" stroke-width="15"/>
+</svg>`;
+const x = `<svg id='x' xmlns="http://www.w3.org/2000/svg" width="238" height="222" viewBox="0 0 238 222" fill="none">
+<line x1="7.5" y1="-7.5" x2="304.373" y2="-7.5" transform="matrix(-0.737479 0.67537 -0.685799 -0.727791 230 3)" stroke="white" stroke-width="15" stroke-linecap="round"/>
+<line x1="7.5" y1="-7.5" x2="304.373" y2="-7.5" transform="matrix(0.737479 0.67537 -0.685799 0.727791 1.52588e-05 8.37018)" stroke="white" stroke-width="15" stroke-linecap="round"/>
+</svg>`;
 let array = [
   [0, 0, 0],
   [0, 0, 0],
@@ -14,13 +22,20 @@ let player;
 let ai;
 const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
+async function setTimer(time) {
+  return new Promise((resolve, reject) => {
+    if (!time) return reject("No time to wait for");
+    setTimeout(() => resolve(time), time * 1000);
+  });
+}
+
 function decidePlayer() {
   player = random(1, 2) === 1 ? 1 : 2; // rand
   player = 2;
   // you are "O"
   ai = player == 1 ? 2 : 1;
   // replaced X O
-  playerTitle.textContent = `You Are "${player == 1 ? "O" : "X"}"`;
+  playerTitle.textContent = `You are "${player == 1 ? "O" : "X"}"`;
 }
 
 function clear() {
@@ -40,7 +55,7 @@ function replay() {
 function displayResult(result) {
   console.log(result);
   resultBox.textContent =
-    result == 0 ? "Toe" : player == result ? "You Win!" : "You Lose!";
+    result == 0 ? "Нічия" : player == result ? "Перемога!" : "Халепа!";
   resultBox.style.opacity = 1;
   background.style.backgroundColor =
     result == 0
@@ -117,18 +132,23 @@ function computerQueue() {
   }
   array[bestMove.i][bestMove.j] = ai;
 }
+const newspaperSpinning = [{ opacity: "0" }, { opacity: "1" }];
 
+const newspaperTiming = {
+  duration: 1000,
+  // iterations: 1,
+};
 function render(array) {
   const newArr = [...array[0], ...array[1], ...array[2]];
-  newArr.forEach((el, i) =>
-    boxes[i].insertAdjacentText(
-      "afterbegin",
-      el == 1 ? "O" : el == 2 ? "X" : ""
-    )
-  );
+  newArr.forEach((el, i) => {
+    if (boxes[i].childNodes.length > 0) return;
+    boxes[i].insertAdjacentHTML("afterbegin", el == 1 ? o : el == 2 ? x : "");
+    if (boxes[i].childNodes.length == 0) return;
+    boxes[i].childNodes[0].style.opacity = 1;
+  });
 }
 
-function run(event) {
+async function run(event) {
   let result = isSolved(array);
   if (result >= 0) return;
   if (!player) decidePlayer();
@@ -142,13 +162,16 @@ function run(event) {
     return;
   array[Math.trunc(number / 3.1)][number - 1 - 3 * Math.trunc(number / 3.1)] =
     player == 1 ? 1 : 2;
-  clear();
+  // clear();
   result = isSolved(array);
   // calc emptySpace
+  render(array);
+  await setTimer(0.5);
 
   player == result ? null : computerQueue();
   result = isSolved(array);
   if (result >= 0) displayResult(result);
+  // clear();
   render(array);
   // check how many empty
 }
